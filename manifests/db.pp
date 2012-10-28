8e63719d1a163a7bcc6712bed8b845c7eef29fa2
Mike Skalnik <mike.skalnik@gmail.com>
2012-10-27 17:24:13 -0700

    Unhypenate exec names
    Not sure why mysql-db-${name} was hyphenated in the first place, but I think
    these names make more sense.

--- manifests/db.pp
+++ manifests/db.pp
@@ -2,13 +2,13 @@ define mysql::db($ensure = present) {
   require mysql
 
   if $ensure == 'present' {
-    exec { "mysql-db-${name}":
+    exec { "create mysql db ${name}":
       command => "mysqladmin -uroot create ${name}",
       creates => "${mysql::config::datadir}/${name}",
       require => Exec['wait-for-mysql']
     }
   } elsif $ensure == 'absent' {
-    exec { "delete-mysql-db-${name}":
+    exec { "delete mysql db ${name}":
       command => "mysqladmin -uroot drop database ${name}",
       require => Exec['wait-for-mysql']
     }
