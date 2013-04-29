242a1fa949958c79d674a4195e91b03f16441642
Alan Norton <me@alannorton.com>
2013-04-29 14:45:21 -0400

    allow user creation at time of database creation.
    I'm not sure if this is the best place to do this, but for now it'll work.

--- manifests/db.pp
+++ manifests/db.pp
@@ -5,7 +5,7 @@
 # Examples
 #
 #   mysql::db { 'foo': }
-define mysql::db($ensure = present) {
+define mysql::db($ensure = present, $user = absent, $password = 'password', $host = '%', $grant = 'ALL') {
   require mysql
 
   if $ensure == 'present' {
@@ -14,10 +14,27 @@ define mysql::db($ensure = present) {
       creates => "${mysql::config::datadir}/${name}",
       require => Exec['wait-for-mysql']
     }
+
+    if $user != 'absent' {
+      exec { "create user ${user} with ${grant} permissions on ${host} to ${name}":
+        command => "mysql -uroot -e \"CREATE USER '${user}'@$'${host}' IDENTIFIED BY '${password}'; GRANT ${grant} PRIVILEGES ON ${name} . * TO '${user}'@$'${host}'; FLUSH PRIVILEGES;\"",
+        require => Exec['wait-for-mysql']
+      }
+    }
+
   } elsif $ensure == 'absent' {
     exec { "delete mysql db ${name}":
       command => "mysqladmin -uroot drop database ${name}",
       require => Exec['wait-for-mysql']
     }
+
+
+    if $user != 'absent' {
+      exec { "drop user ${user}@${host}":
+        command => "mysql -uroot -e \"GRANT USAGE ON *.* TO '${user}'@'${host}'; DROP USER '${user}'@'${host}'\"",
+        require => Exec['wait-for-mysql']
+      }
+    }
+
   }
 }
--- spec/defines/db_spec.rb
+++ spec/defines/db_spec.rb
@@ -22,6 +22,30 @@ describe 'mysql::db' do
     end
   end
 
+  context "when ensure and user are present" do
+    let(:params) do
+      { :ensure => 'present',
+        :user => "dbuser" }
+    end
+
+    it { should include_class('mysql') }
+
+    it "creates the database" do
+      should contain_exec("create mysql db #{title}").
+             with(
+               :command => "mysqladmin -uroot create #{title}",
+               :creates => "/opt/boxen/data/mysql/#{title}"
+             )
+    end
+
+    it "creates the correct user" do
+      should contain_exec("create user dbuser with ALL permissions on % to #{title}").
+             with(
+               :command => "mysql -uroot -e \"CREATE USER 'dbuser'@$'%' IDENTIFIED BY 'password'; GRANT ALL PRIVILEGES ON #{title} . * TO 'dbuser'@$'%'; FLUSH PRIVILEGES;\""
+             )
+    end
+  end
+
   context "when ensure is absent" do
     let(:params) do
       { :ensure => 'absent' }
