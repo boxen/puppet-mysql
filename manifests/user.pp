251a542e418800e70a02f43c72aceccaa0f28aa1
Tom Bell <tomb@tomb.io>
2013-09-09 17:13:10 +0100

    add missing pipes

--- manifests/user.pp
+++ manifests/user.pp
@@ -21,8 +21,8 @@ define mysql::user($ensure = present,
         -e \"create user '${name}'@'${host}' identified by '${password}'; \"",
       require => Exec['wait-for-mysql'],
       unless  => "mysql -uroot -p${mysql::config::port} --password='' \
-        -e 'SELECT User,Host FROM mysql.user;' \
-        grep -w '${name}' | grep -w '${host}'"
+        -e 'SELECT User,Host FROM mysql.user;'\
+        | grep -w '${name}' | grep -w '${host}'"
     }
   } elsif $ensure == 'absent' {
     exec { "delete mysql user ${name}":
--- manifests/user/grant.pp
+++ manifests/user/grant.pp
@@ -32,7 +32,7 @@ define mysql::user::grant($database,
       require => Exec['wait-for-mysql'],
       unless  => "mysql -uroot -p${mysql::config::port} --password='' \
         -e 'SHOW GRANTS FOR ${username}@${host};' \
-        grep -w '${database}' | grep -w '${grants}'"
+        | grep -w '${database}' | grep -w '${grants}'"
     }
   } elsif $ensure == 'absent' {
     exec { "removing ${username} access to ${database}":
