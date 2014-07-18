7cb98cb3850ac9f7c5e63c733e499d499c0dbdf0
Alan Norton <alan@betterment.com>
2014-07-18 15:38:30 -0400

    remove port specification

--- manifests/db.pp
+++ manifests/db.pp
@@ -10,7 +10,7 @@ define mysql::db($ensure = present, $user = absent, $password = 'password', $hos
 
   if $ensure == 'present' {
     exec { "create mysql db ${name}":
-      command => "mysqladmin -uroot -p13306 create ${name} --password=''",
+      command => "mysqladmin -uroot create ${name} --password=''",
       creates => "${mysql::config::datadir}/${name}",
       require => Exec['wait-for-mysql'],
       unless  => "mysql -uroot -p13306 -e 'show databases' \
@@ -29,7 +29,7 @@ define mysql::db($ensure = present, $user = absent, $password = 'password', $hos
 
   } elsif $ensure == 'absent' {
     exec { "delete mysql db ${name}":
-      command => "mysqladmin -uroot -p13306 drop ${name} --password=''",
+      command => "mysqladmin -uroot drop ${name} --password=''",
       require => Exec['wait-for-mysql']
     }
 
