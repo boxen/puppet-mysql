8db49fe534631ce29725dad58e597da7cfe61145
Zach Davis <zach@castironcoding.com>
2018-04-13 06:24:29 -0700

    Remove deprecated undents

--- files/brews/mysql.rb
+++ files/brews/mysql.rb
@@ -123,7 +123,7 @@ class Mysql < Formula
     end
   end
 
-  def caveats; <<-EOS.undent
+  def caveats; <<~EOS
     A "/etc/my.cnf" from another install may interfere with a Homebrew-built
     server starting up correctly.
 
@@ -134,7 +134,7 @@ class Mysql < Formula
 
   plist_options :manual => "mysql.server start"
 
-  def plist; <<-EOS.undent
+  def plist; <<~EOS
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
